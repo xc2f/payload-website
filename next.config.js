aeb5f3ae3180df94b8b6da1c739c1ed007adbc2d
Fen <hellowd93@163.com>
2026-01-10 20:05:37 +0800

    add db

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
-  output: 'standalone',
+  output: process.env.BUILD_OUTPUT || undefined,
   typescript: {
     ignoreBuildErrors: true,
   },
