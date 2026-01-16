303c382fdf01965e8bd33ef6c5153b84177e87ac
Fen <hellowd93@163.com>
2026-01-16 20:43:48 +0800

    update ci

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,6 @@
-import * as migration_20260106_184630 from './20260106_184630';
-import * as migration_20260107_045216 from './20260107_045216';
-import * as migration_20260111_154244 from './20260111_154244';
-import * as migration_20260115_132347 from './20260115_132347';
+import * as migration_20260106_184630 from './20260106_184630'
+import * as migration_20260107_045216 from './20260107_045216'
+import * as migration_20260111_154244 from './20260111_154244'
 
 export const migrations = [
   {
@@ -19,9 +18,4 @@ export const migrations = [
     down: migration_20260111_154244.down,
     name: '20260111_154244',
   },
-  {
-    up: migration_20260115_132347.up,
-    down: migration_20260115_132347.down,
-    name: '20260115_132347'
-  },
-];
+]
